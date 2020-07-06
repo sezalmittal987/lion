0fcfdbfb26065838fbd33b078f1d613ef9d6ea98
Joren Broekema <joren.broekema@ing.com>
2020-07-06 12:33:32 +0200

    chore: type rest of lion core

--- packages/core/src/UpdateStylesMixin.d.ts
+++
@@ -1,5 +0,0 @@
-export type UpdateStylesMixin = typeof import("../types/UpdateStylesMixinTypes").UpdateStylesMixinImplementation;
-export const UpdateStylesMixin: (s: import("@open-wc/dedupe-mixin").Constructor<{}>) => any;
-export type StylesMap = {
-    [key: string]: string;
-};
