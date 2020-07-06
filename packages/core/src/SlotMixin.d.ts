10ab83a469190e63900287a7fd13d74dcb16af7e
Joren Broekema <joren.broekema@ing.com>
2020-07-06 16:35:41 +0200

    chore: remove auto-generated slotmixin type file from git

--- packages/core/src/SlotMixin.d.ts
+++
@@ -1,5 +0,0 @@
-export type SlotMixin = typeof import("../types/SlotMixinTypes").SlotMixinImplementation;
-export const SlotMixin: (s: import("@open-wc/dedupe-mixin").Constructor<{}>) => any;
-export type SlotsMap = {
-    [key: string]: typeof import("../types/SlotMixinTypes").slotFunction;
-};
